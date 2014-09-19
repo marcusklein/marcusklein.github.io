3bd188f68f8bae42831bcc2909d055c7e8426206
marcusklein <marcusklein@me.com>
2014-09-19 14:16:24 +1200

    styled, V1.0 complete

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 <html>
 <head>
 	<title>Marcus Klein's GitHub</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,800' rel='stylesheet' type='text/css'>
 
 	<style type="text/css">
 		body {
 			margin: 0;
+			font-family: 'Open Sans', sans-serif;
+			font-weight: 800;
 		}
 
 		#exe {
 			text-align: center;
+			padding-top: 50%;
 			margin: auto;
 			width: 200px;
 			height: 50px;
@@ -34,13 +38,14 @@
 </head>
 <body>
 	<div id="exe">
-		<a href="http://executable.co">executable.co</a>
+		<a href="http://executable.co"><h1>executable.co</h1></a>
+		hello world
 	</div>
 	<div id="shared">
-		<a href="https://github.com/marcusklein">Shared GitHub</a>
+		<a href="https://github.com/marcusklein"><h1>Shared GitHub</h1></a>
 	</div>	
 	<div id="personal">
-		<a href="https://github.com/monodromy">Private GitHub</a>
+		<a href="https://github.com/monodromy"><h1>Private GitHub</h1></a>
 	</div>
 </body>
 </html>
